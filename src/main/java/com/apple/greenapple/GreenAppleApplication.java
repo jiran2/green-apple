7774e7d7a66dd573ff5039f8e5b51bdd09d26ae0
jiran01 <jiran@airecruitas.com>
2020-11-02 18:31:45 +0800

    初始化项目

---
+++ src/main/java/com/apple/greenapple/GreenAppleApplication.java
@@ -0,0 +1,13 @@
+package com.apple.greenapple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenAppleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenAppleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apple/greenapple/GreenAppleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apple.greenapple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenAppleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
